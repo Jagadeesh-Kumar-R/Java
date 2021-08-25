870a0ad4dfe67ac52b6292a2bb600add6354b64e
Jagadeesh-Kumar-R <73772420+Jagadeesh-Kumar-R@users.noreply.github.com>
2021-08-25 17:30:20 +0530

    Sort elements by position
    sort the elements in odd positions in descending order and elements in ascending order
    
    Eg 1: Input: 13,2 4,15,12,10,5
            Output: 13,2,12,10,5,15,4
    Eg 2: Input: 1,2,3,4,5,6,7,8,9
            Output: 9,2,7,4,5,6,3,8,1

---
+++ SortWithPosition.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Main{
+    public static void SortWithPosition(String[] args){
+        Scanner scan=new Scanner(System.in);
+        String s=scan.next();
+        String[] str=s.split(",");
+        ArrayList<Integer> list1=new ArrayList<Integer>();
+        ArrayList<Integer> list2=new ArrayList<Integer>();
+        for(int i=0;i<str.length;i++){
+            if(i%2==0){
+                list1.add(Integer.parseInt(str[i]));
+            }else{
+                list2.add(Integer.parseInt(str[i]));
+            }
+        }
+        Collections.sort(list1);
+        Collections.sort(list2);
+        int ans[]=new int[str.length];
+        for(int i=0;i<list1.size();i++){
+            ans[i*2]=list1.get(list1.size()-i-1);
+        }
+        for(int i=0;i<list2.size();i++){
+            ans[i*2+1]=list2.get(i);
+        }
+        for(int i=0;i<str.length;i++){
+            System.out.print(ans[i]+",");
+        }
+        
+    }
+}
